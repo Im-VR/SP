1bb03015995ed3a2d702c5a68fb801ed6798b414
Rimaz AlGhamdi <rimazsaleh@gmail.com>
2018-03-04 15:30:25 +0300

    level1
    (3/4/2018)

---
+++ IM/Assets/Cardboard/Scripts/VRWalk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRWalk : MonoBehaviour {
+    public Transform VRCam;
+	public float ToggleAngle = 30.0f;
+	public float speed = 3.0f;
+	public bool moveF;
+	private CharacterController cc;
+	// Use this for initialization
+	void Start () {
+		cc = GetComponent<CharacterController> ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (VRCam.eulerAngles.x >= ToggleAngle && VRCam.eulerAngles.x < 90.0f) {
+			moveF = true;
+		} else {
+			moveF = false;
+		}
+		if (moveF == true) {
+			Vector3 forward = VRCam.TransformDirection (Vector3.forward);
+			cc.SimpleMove (forward * speed);
+		}
+		
+	}
+}
---
+++ IM/Assets/timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime;
+    private bool finnished = false;
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (finnished)
+            return;
+        
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string sec = (t % 60).ToString("f0");
+
+        timerText.text = "Timer: " + minutes + " : " + sec;
+    }
+
+    public void Finnish(){
+        finnished = true;
+        timerText.color = Color.yellow;
+    }
+}
